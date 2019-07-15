a6cb4eebeff08956ebee6b9f83938aa76045ee1e
Alexandre Yang <alexandreyang.yang@gmail.com>
2019-07-15 16:08:19 +0200

    error j'ai mergé la task directement dans develop

--- calc.js
+++ calc.js
@@ -1,23 +1,23 @@
 var mathjs = require('mathjs');
 
-exports.add = function(x,y) {
+exports.add = function (x, y) {
     return mathjs.add(x, y);
 }
 
-exports.subtract = function(x,y) {
+exports.subtract = function (x, y) {
     return mathjs.subtract(x, y);
 }
 
-exports.multiply = function(x,y) {
+exports.multiply = function (x, y) {
     return mathjs.multiply(x, y);
 }
 
-exports.divide = function(x,y) {
+exports.divide = function (x, y) {
     return mathjs.divide(x, y);
 }
 
-exports.abs = function(x) {
-    if (x < 0){
+exports.abs = function (x) {
+    if (x < 0) {
         x = x * -1;
     };
     return x;
